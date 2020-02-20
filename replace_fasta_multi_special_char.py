e2dfd7aba5ac8c62a30bd1e43f00047ca9df3ac3
svpipaliya <61172011+svpipaliya@users.noreply.github.com>
2020-02-19 20:46:41 -0700

    Add files via upload

--- replace_fasta_multi_special_char.py
+++ replace_fasta_multi_special_char.py
@@ -1,9 +1,42 @@
 #!/usr/bin/env python
+## This script reads and removes a single specified special character from a string
+## Ask Shweta if you are confused
 
-# This script reads and removes special characters from a fasta file
-import re
-original_fasta = open('/Users/ShwetaPipaliya/Desktop/naeg_JGI.fa').read()
-new_fasta = re.sub('[|]', '_', original_fasta)
-open('n1.fa', 'w').write(new_fasta)
+#Replace a set of multiple sub strings with a new string in main string.
 
-#end
+mainStr = "Hello, This is a sample string"
+def replaceMultiple(mainString, toBeReplaces, newString):
+    # Iterate over the strings to be replaced
+    for elem in toBeReplaces :
+        # Check if string is in the main string
+        if elem in mainString :
+            # Replace the string
+            mainString = mainString.replace(elem, newString)
+    
+    return  mainString     
+ 
+def main():
+    
+    mainStr = "Hello, This is a sample string"
+ # Replace all occurrences of given character or string in main string
+    otherStr = mainStr.replace('s' , 'X') 
+     
+    print("String with replaced Content : " , otherStr) 
+    
+    #Replace First 2 occurrences of given character or string in main string
+    otherStr = mainStr.replace('s' , 'XXXS', 2) 
+     
+    print(otherStr) 
+    
+    
+#Replace multiple characters / strings from a string
+  
+    # Replace all the occurrences of string in list by AA in the main list 
+    otherStr = replaceMultiple(mainStr, ['s', 'l', 'a'] , "AA")
+    
+    print(otherStr)
+           
+if __name__ == '__main__':
+    main()
+    
+##End
