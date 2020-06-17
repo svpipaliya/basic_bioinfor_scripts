8f1fe6fb7f42e35d8674187af6d697dce5c3a14a
svpipaliya <61172011+svpipaliya@users.noreply.github.com>
2020-06-17 13:11:24 -0600

    Update phylobayes_cedar_fullnode.sh

--- phylogenetics-on-cluster/phylobayes_cedar_fullnode.sh
+++ phylogenetics-on-cluster/phylobayes_cedar_fullnode.sh
@@ -12,7 +12,7 @@ module load intel/2018.3
 module load openmpi/3.1.2
 module load phylobayes-mpi/20180420
 
-#phylobayes on MPI, nthreads set to 8 (refer to PB manual to alternative options), CAT-GTR model (use LG if CAT not necessary)
+#phylobayes on MPI, nthreads set to 8 (refer to PB manual for alternatives), CAT-GTR model (use LG if CAT not necessary)
 mpirun -n 8 pb_mpi -d example.phy -cat -gtr example_out
 
 ##End Script
