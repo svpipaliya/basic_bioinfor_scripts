b5554565e20723653aebe32c17f3060a20ab6a75
svpipaliya <61172011+svpipaliya@users.noreply.github.com>
2020-06-17 13:12:52 -0600

    Update raxml_cedar_partialnode.sh

--- phylogenetics-on-cluster/raxml_cedar_partialnode.sh
+++ phylogenetics-on-cluster/raxml_cedar_partialnode.sh
@@ -11,6 +11,7 @@ module load intel/2018.3
 module load openmpi/3.1.2
 module load raxml/8.2.11
 
+#raxml using MPI, nthreads set to 8 (refer to RAxML on parallel processing in manual for alternative), model LG4X using gamma distribution, seed setting random
 mpirun -n 8 raxmlHPC-MPI -f d -m PROTGAMMALG4X -s example-phy -# 100 -p 12345 -b 12345 -n MultipleBootstrap
 
 ##End Script
